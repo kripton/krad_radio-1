88c9cbd7114c597f09591dfcf4c2eec9d6a0d8fb
David Richards <kradradio@gmail.com>
2014-01-29 03:45:07 -0800

    typo fix

--- lib/krad_transponder/krad_transponder.c
+++ lib/krad_transponder/krad_transponder.c
@@ -93,10 +93,10 @@ static void xpdr_adapter_event_cb(kr_adapter_event_cb_arg *arg) {
   while ((path = kr_pool_iterate_active(xpdr->path_pool, &i))) {
     if (path->info.output.type == KR_XPDR_ADAPTER) {
       if (path->info.output.type == KR_XPDR_COMPOSITOR) {
-        kr_compositor_path_process(path->input.compositor_path);
+        kr_compositor_process(path->input.compositor_path);
       }
       if (path->info.input.type == KR_XPDR_MIXER) {
-        kr_mixer_path_process(path->input.mixer_path);
+        kr_mixer_process(path->input.mixer_path);
       }
       test++;
     }
