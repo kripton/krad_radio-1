5252b06477899d8c7f281c7a8b54d7959ec579b7
David Richards <kradradio@gmail.com>
2014-02-04 23:27:03 -0800

    italian code

--- tests/kr_json_patch_test.c
+++ tests/kr_json_patch_test.c
@@ -16,6 +16,18 @@
 #define KR_PATCH_DEPTH_MAX 8
 #define KR_PATCHSET_PATCHES_MAX 128
 
+#define KR_JSON_TOKR_MAX 512
+typedef jsmntok_t kr_jtok;
+
+typedef struct {
+  jsmntok_t tokens[KR_JSON_TOKR_MAX];
+  int cur;
+  int ret;
+  int sz;
+  char *json;
+  int json_len;
+} kr_jtokr;
+
 typedef struct kr_patch kr_patch;
 
 struct kr_patch {
@@ -28,6 +40,91 @@ struct kr_patchset {
   kr_patch patches[KR_PATCHSET_PATCHES_MAX];
   int used;
 };
+
+
+int kr_jtokr_init(kr_jtokr *tokr) {
+  if (tokr == NULL) return -1;
+  memset(tokr, 0, sizeof(*tokr));
+  tokr->sz = KR_JSON_TOKR_MAX;
+  return tokr->sz;
+}
+
+kr_jtok *kr_jtokr_iter(kr_jtokr *tokr) {
+  if (tokr == NULL) return NULL;
+  if (tokr->cur < tokr->ret) {
+    return &tokr->tokens[tokr->cur++];
+  }
+  return NULL;
+}
+
+kr_jtok *kr_jtokr_iter_limit(kr_jtokr *tokr, int limit) {
+  if (tokr == NULL) return NULL;
+  if (tokr->cur < tokr->ret) {
+    if (tokr->tokens[tokr->cur].end <= limit) {
+      return &tokr->tokens[tokr->cur++];
+    }
+  }
+  return NULL;
+}
+
+int kr_jtokr_tseek(kr_jtokr *tokr, int pos) {
+  if (tokr == NULL) return -1;
+  if (pos < 0) return -1;
+  if (pos < tokr->ret) {
+    tokr->cur = pos;
+    return 0;
+  }
+  return -1;
+}
+
+int kr_jtokr_reltseek(kr_jtokr *tokr, int pos) {
+  if (tokr == NULL) return -1;
+  pos += tokr->cur;
+  return kr_jtokr_tseek(tokr, pos);
+}
+
+kr_jtok *kr_jtokr_overseek(kr_jtokr *tokr, int pos) {
+  kr_jtok *tok;
+  if (tokr == NULL) return NULL;
+  if (pos < 0) return NULL;
+  while ((tok = kr_jtokr_iter(tokr))) {
+    if (tok->start > pos ) {
+      /*kr_jtokr_reltseek(tokr, -1);*/
+      return tok;
+    }
+  }
+  return NULL;
+}
+
+int kr_jtokr_rewind(kr_jtokr *tokr) {
+  if (tokr == NULL) return -1;
+  kr_jtokr_tseek(tokr, 0);
+  return 0;
+}
+
+kr_jtokr *kr_json_parse(kr_jtokr *tokr, char *json, int len) {
+  jsmn_parser parser;
+  jsmnerr_t err;
+  if ((tokr == NULL) || (json == NULL) || (len < 1)) return NULL;
+  jsmn_init(&parser);
+  kr_jtokr_init(tokr);
+  err = jsmn_parse(&parser, json, tokr->tokens, tokr->sz);
+  tokr->ret = parser.toknext;
+  tokr->json = json;
+  tokr->json_len = len;
+  if (err != JSMN_SUCCESS || tokr->ret < 3) {
+    return NULL;
+  }
+  return tokr;
+}
+
+int kr_jtokstrcmp(kr_jtokr *tokr, kr_jtok *tok, char *str, int len) {
+ int strsz;
+ strsz = tok->end - tok->start;
+ //printf("we got len %d and %d\n", strsz, len);
+ if (len != strsz) return -1;
+ return memcmp(str, &tokr->json[tok->start], len);
+}
 
 static void path_print(kr_path *path) {
   char *name;
@@ -51,56 +148,22 @@ static void path_print(kr_path *path) {
   printf("Last is (%d chars) %s\n", len, name);*/
 }
 
-int json_to_patchset(kr_path *path, char *json, int json_len, kr_patchset *patchset) {
+int handle_json_partial(kr_patchset *patchset, kr_path *path, kr_jtokr *tokr, int limit) {
+  kr_jtok *tok;
   int ret;
-  jsmn_parser parser;
-  jsmntok_t tokens[512];
-  jsmntok_t *tok;
-  jsmnerr_t err;
-  int ntokens;
-  int k;
-  int c;
   int p;
+  int c;
   union {
     int integer;
     float real;
   } value;
-  if ((path == NULL) || (json == NULL) || (json_len < 1) || (patchset == NULL)) {
-    return -1;
-  }
-  printf("Going to parse:\n%s\n", json);
-  if (0) {
-    path_print(path);
-    kr_path_rewind(path);
-  }
-  k = 0;
   ret = 0;
-  patchset->used = 0;
-  jsmn_init(&parser);
-  err = jsmn_parse(&parser,json,tokens,512);
-  ntokens = parser.toknext;
-  if (err != JSMN_SUCCESS || ntokens < 3) {
-    return -1;
-  }
-  //printf("We got %d tokens\n", ntokens);
-  tok = &tokens[k];
-  if (tok->type == JSMN_OBJECT) {
-    printf("First we got an object!!\n");
-  } else {
-    if (tok->type == JSMN_ARRAY) {
-      printf("First we got an array!!\n");
-    } else {
-      printf("First we went wrong.. bailing..\n");
-      return -1;
-    }
-  }
-  for (k = 1; k < ntokens; k++) {
+  while ((tok = kr_jtokr_iter_limit(tokr, limit))) {
     if (patchset->used == KR_PATCHSET_PATCHES_MAX) {
       /* damn this would be attacker to fail */
       printf("Too many patches\n");
       return -1;
     }
-    tok = &tokens[k];
     switch (tok->type) {
       case JSMN_OBJECT:
         //printf("Token %d an object\n", k + 1);
@@ -115,7 +178,7 @@ int json_to_patchset(kr_path *path, char *json, int json_len, kr_patchset *patch
           printf("Too many paths\n");
           return -1;
         }
-        kr_path_push(path, &json[tok->start], tok->end - tok->start);
+        kr_path_push(path, &tokr->json[tok->start], tok->end - tok->start);
         if (0) {
           path_print(path);
           kr_path_rewind(path);
@@ -129,12 +192,12 @@ int json_to_patchset(kr_path *path, char *json, int json_len, kr_patchset *patch
         }
         p = 0;
         for (c = tok->start; c < tok->end; c++) {
-          if ((!isdigit(json[c])) && (json[c] != '.')) {
+          if ((!isdigit(tokr->json[c])) && (tokr->json[c] != '.')) {
             /* rage force disconnect the fool who sends a non number */
             printf("Got a primitive that isn't a number, ragequiting\n");
             return -1;
           }
-          if (json[c] == '.') {
+          if (tokr->json[c] == '.') {
            if (p == 1) {
              printf("Too many points in your floating bullshit\n");
              return -1;
@@ -148,11 +211,11 @@ int json_to_patchset(kr_path *path, char *json, int json_len, kr_patchset *patch
           kr_path_rewind(path);
         }
         if (p == 1) {
-          value.real = atof(&json[tok->start]);
+          value.real = atof(&tokr->json[tok->start]);
           printf("We got a %f floater wa doo!\n", value.real);
         }
         if (p == 0) {
-          value.integer = atoi(&json[tok->start]);
+          value.integer = atoi(&tokr->json[tok->start]);
           printf("We got a %d integer wee hoo!\n", value.integer);
         }
         //ret = kr_strarr_to_patch(&patchset->patches[patchset->used], value, path, path_len);
@@ -172,6 +235,121 @@ int json_to_patchset(kr_path *path, char *json, int json_len, kr_patchset *patch
   printf("Done parsing json patchset, we got %d patches\n\n", patchset->used);
   return patchset->used;
 }
+
+int handle_json_patch_obj(kr_patchset *patchset, kr_path *path, kr_jtokr *tokr) {
+  int or;
+  int ret;
+  int oseek;
+  char *patch_path;
+  char *value;
+  int vstart;
+  int vlimit;
+  //int vlen;
+  kr_jtok *tok;
+  if (tokr == NULL) return -1;
+  oseek = 0;
+  or = 0;
+  vstart = 0;
+  vlimit = 0;
+  //vlen = 0;
+  patch_path = NULL;
+  value = NULL;
+  tok = kr_jtokr_iter(tokr);
+  if (tok == NULL) return -3;
+  if (tok->type != JSMN_OBJECT) return -1;
+  printf("patch obj size %d\n", tok->size);
+  if (tok->size != 6) return -2;
+  for (;;) {
+    if ((or == 1) && (patch_path != NULL) && (value != NULL)) {
+      printf("winner!\n");
+      kr_jtokr_tseek(tokr, vstart);
+      ret = handle_json_partial(patchset, path, tokr, vlimit);
+      printf("handle json partial ret %d\n", ret);
+      return 0;
+    }
+    if (oseek != 0) {
+      tok = kr_jtokr_overseek(tokr, oseek);
+      oseek = 0;
+    } else {
+      tok = kr_jtokr_iter(tokr);
+    }
+    switch (tok->type) {
+      case JSMN_STRING:
+        if ((or == 0) && (kr_jtokstrcmp(tokr, tok, "op", 2) == 0)) {
+          tok = kr_jtokr_iter(tokr);
+          if (kr_jtokstrcmp(tokr, tok, "replace", 7) == 0) {
+            //printf("we got op replace!!\n");
+            or = 1;
+            continue;
+          } else {
+            printf("not a op replace\n");
+            return -5;
+          }
+        }
+        if ((patch_path == NULL) && (kr_jtokstrcmp(tokr, tok, "path", 4) == 0)) {
+          tok = kr_jtokr_iter(tokr);
+          //printf("we got path\n");
+          patch_path = &tokr->json[tok->start];
+          continue;
+        }
+        if ((value == NULL) && (kr_jtokstrcmp(tokr, tok, "value", 5) == 0)) {
+          tok = kr_jtokr_iter(tokr);
+          //printf("we got value\n");
+          value = &tokr->json[tok->start];
+          printf("we have %d babies\n", tok->size);
+          vstart = tokr->cur;
+          vlimit = tok->end;
+          if ((or != 1) || (patch_path == NULL)) {
+            oseek = tok->end;
+            //printf("will seek %d\n", oseek);
+          }
+          continue;
+        }
+        /* Falling thru if we catch nothing */
+      default:
+        printf("we went wrong.. bailing..\n");
+        return -1;
+    }
+  }
+  return -4;
+}
+
+
+int json_to_patchset(kr_path *path, char *json, int json_len, kr_patchset *patchset) {
+  int ret;
+  kr_jtokr tokr;
+  kr_jtok *tok;
+  if ((path == NULL) || (json == NULL) || (json_len < 1) || (patchset == NULL)) {
+    return -1;
+  }
+  printf("Going to parse:\n%s\n", json);
+  if (0) {
+    path_print(path);
+    kr_path_rewind(path);
+  }
+  ret = 0;
+  patchset->used = 0;
+  kr_json_parse(&tokr, json, json_len);
+  tok = kr_jtokr_iter(&tokr);
+  switch (tok->type) {
+    case JSMN_OBJECT:
+      printf("Parsing as JSON partial update\n");
+      ret = handle_json_partial(patchset, path, &tokr, 0);
+      printf("handle json partial ret %d\n", ret);
+      break;
+    case JSMN_ARRAY:
+      printf("Parsing as JSON PATCH Array\n");
+      while ((ret = handle_json_patch_obj(patchset, path, &tokr) == 0)) {
+        printf("handle json patch obj ret %d\n", ret);
+      }
+      break;
+    default:
+      printf("First we went wrong.. bailing..\n");
+      return -1;
+  }
+  return patchset->used;
+}
+
 
 static int test(char *json_in) {
   int ret;
