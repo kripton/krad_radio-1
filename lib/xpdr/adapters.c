8c4049ea44881672eee3021c1068f9d9c814ee04
David Richards <kradradio@gmail.com>
2014-02-08 05:13:22 -0800

    add adapters func pointer struct array

--- lib/xpdr/adapters.c
+++ lib/xpdr/adapters.c
@@ -7,6 +7,85 @@
 
 #include "kr_adapter.h"
 
-static const kr_adapter_spec adapters[1] = {
+/* Must match the kr_xpdr_type enum */
 
+static const kr_adapter_spec adapters[] = {
+  { /* First is NULL */
+    .pctl = NULL,
+    .rmpath = NULL,
+    .mkpath = NULL,
+    .ctl = NULL,
+    .destroy = NULL,
+    .create = NULL
+  },{ /* KR_JACK */
+    .pctl = NULL,
+    .rmpath = NULL,
+    .mkpath = NULL,
+    .ctl = NULL,
+    .destroy = NULL,
+    .create = NULL
+  },{ /* KR_WAYLAND */
+    .pctl = NULL,
+    .rmpath = NULL,
+    .mkpath = NULL,
+    .ctl = NULL,
+    .destroy = NULL,
+    .create = NULL
+  },{ /* KR_V4L2 */
+    .pctl = NULL,
+    .rmpath = NULL,
+    .mkpath = NULL,
+    .ctl = NULL,
+    .destroy = NULL,
+    .create = NULL
+  },{ /* KR_DECKLINK */
+    .pctl = NULL,
+    .rmpath = NULL,
+    .mkpath = NULL,
+    .ctl = NULL,
+    .destroy = NULL,
+    .create = NULL
+  },{ /* KR_X11 */
+    .pctl = NULL,
+    .rmpath = NULL,
+    .mkpath = NULL,
+    .ctl = NULL,
+    .destroy = NULL,
+    .create = NULL
+  },{ /* KR_ALSA */
+    .pctl = NULL,
+    .rmpath = NULL,
+    .mkpath = NULL,
+    .ctl = NULL,
+    .destroy = NULL,
+    .create = NULL
+  },{ /* KR_AUX */
+    .pctl = NULL,
+    .rmpath = NULL,
+    .mkpath = NULL,
+    .ctl = NULL,
+    .destroy = NULL,
+    .create = NULL
+  },{ /* KR_ */
+    .pctl = NULL,
+    .rmpath = NULL,
+    .mkpath = NULL,
+    .ctl = NULL,
+    .destroy = NULL,
+    .create = NULL
+  },{ /* KR_ */
+    .pctl = NULL,
+    .rmpath = NULL,
+    .mkpath = NULL,
+    .ctl = NULL,
+    .destroy = NULL,
+    .create = NULL
+  },{ /* KR_ */
+    .pctl = NULL,
+    .rmpath = NULL,
+    .mkpath = NULL,
+    .ctl = NULL,
+    .destroy = NULL,
+    .create = NULL
+  }
 };
