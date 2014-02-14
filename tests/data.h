4856bc4a8808e189a3520d7f9d2b4b28669d7b13
David Richards <kradradio@gmail.com>
2014-02-14 01:33:49 -0800

    add some testing data

---
+++ tests/data.h
@@ -0,0 +1,22 @@
+static const char *names[] = {
+  "idomeneo",  "subduct",  "italianating",  "dromos",  "donets",
+  "crystallinity",  "arborescently",  "privately",  "expatriating",
+  "granicus",  "anacoustic",  "washerwoman",  "wadsetting",  "saviorship",
+  "justification",  "scrollwork",  "hypnotization",  "succorer",  "papula",
+  "prediscrimination",  "bugbane",  "decca",  "christoff",  "steamless",
+  "compromissary",  "aves",  "docket",  "crookback",  "tuberculinize",
+  "lopstick",  "aircraftsman",  "chelicera",  "chunnel",  "spiderman",
+  "nurl",  "prepriced",  "lawful",  "reillumination",  "nicaragua",
+  "reaccuse",  "poussin",  "unshocked",  "gnaw",  "hammerfest",  "privates",
+  "nonstimulant",  "unmeasurably",  "shemite",  "arctogaeic",  "school",
+  "intertribal",  "meningococci",  "octavia",  "penultimate",  "netsuke",
+  "abed",  "unsoiled",  "rickettsial",  "mousepox",  "bourse",  "unbating",
+  "grackle",  "vulgarised",  "nonrevokable",  "leerily",  "poetizer",  "underscribe",
+  "disposal",  "exstrophy",  "nymph",  "jangly",  "precollector",  "intersexually",
+  "cogitation",  "hazer",  "essay",  "reincarnationist",  "lombardic",
+  "unsplenetic",  "heroicity",  "phdre",  "cofinal",  "backbite",
+  "staphylorrhaphy",  "alopecia",  "entertainment",  "fabre",
+  "precommunicated",  "bastogne",  "starriness",  "multure",
+  "polycarpous",  "peronismo",  "homeotherm",  "thessalonki",
+  "accessibility",  "decidable",  "delitescency",  "biding",  "exilarch"
+};
